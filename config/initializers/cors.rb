5bf4269fe8de833b0bb1742280f31422beba0d5d
Sergey Petrunin <neyasut@gmail.com>
2016-01-20 00:30:27 +0200

    change protect from forgery method; update client app port

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
  Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
    allow do
-     origins 'localhost:9000'
+     origins 'localhost:4200'
 
      resource '*',
        headers: :any,
